05e68a3d6475f5eeba401c3daa1d9f0b491ed847
ahmedFarouk2020 <ahmedfarook0111@gmail.com>
2021-01-08 00:12:31 +0200

    session added

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask
+from datetime import timedelta
 from routes.login import logIn
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = b'ad^&%#HJaS54O%$%' # to encrypte the data
+app.permanent_session_lifetime = timedelta(days=5) # 5-day session
 app.register_blueprint(logIn)
 
-@app.route('/')
+@app.route('/')   
 def index():
-    return render_template("index.html")
+    # if the session exists
+    if 'username' in session:
+        return render_template('index.html')
+    else: 
+        return render_template('loginForm.html')
+
+@app.route('/logout')   
+def logout():
+    # if the session exists
+    if 'username' in session:
+        session.pop('username', None) # remove the session
+        return 'logged out'
+    else: 
+        return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- routes/login.py
+++ routes/login.py
@@ -1,61 +1,60 @@
-from flask import Blueprint,request,render_template,redirect,url_for
+from flask import Blueprint,request,render_template,redirect,url_for, session
+from datetime import timedelta
 import mysql.connector
+
 logIn = Blueprint("login",__name__)
 
-DB = mysql.connector.connect(host="localhost",user="root",passwd="mysql",database="ICUroomsDB")
+
+DB = mysql.connector.connect(host="localhost",user="farook",passwd="sql123",database="ICUroomsDB")
 cursor = DB.cursor()
 @logIn.route('/login',methods=['GET','POST'])
 def login():
     error=""
     if request.method == "POST" :
+        session.permanent = True
         return valid_login(request.form['id'],request.form['password'])
     else :
         return render_template("loginForm.html",error=error) 
 
 #get the id of the user returns -1 if it doesn't exist and returns the id if it exist
 def getID(id) : 
-    getid = "SELECT t.id FROM (SELECT id FROM doctors UNION SELECT id FROM patients) t WHERE id = %s"
+    getid = "SELECT t.id FROM (SELECT id FROM doctors UNION SELECT id FROM patients UNION SELECT id FROM technicians) t WHERE id = %s"
     val = (id,)
     cursor.execute(getid,val)
     if cursor.fetchone() == None :
-        return -1
+        return '-1'
     else :
         return id
 # defining the id to know the user wheather he is a doctor or patien or tech it returns a object of id and the type
 def defining_id (id) :
     idstr = str(id)
-    lastTwoDigits = idstr[len(idstr) - 2] +  idstr[len(idstr) - 1] # now i have the last two digits
+    lastTwoDigits = idstr[0] + idstr[1] # now i have the last two digits
     if lastTwoDigits == "00" :
-        return {'id' : id, 'typ' : "doctors"}
+        return {"id" : id, "typ" : 'doctors'}
     elif lastTwoDigits == "01" :
-        return {'id' : id, 'typ' : "patients"}
-    else :
-        return {'id' : id, 'typ' : "technicians"}
+        return {"id" : id, "typ" : 'patients'}
+    else: #PUUUUUUUUUUUUUUUUG must do a condition
+        return {"id" : id, "typ" : 'technicians'}
     
 # checkin if the id and the password is correct and log in if So
 def valid_login(id,password) :
     error = ""
     ID = getID(id)
-    if ID == -1 :
+    if ID == '-1' :
         error = "this id doesn't exist "
         return render_template("loginForm.html",error=error)
     else :
         idAndType = defining_id(id)
         # checking the password
         # get the password that belongs to that id (real password)
-        getpass = "SELECT PASSWORD FROM %s WHERE id = %s"%(idAndType['typ'],int(idAndType['id']))
+        getpass = "SELECT password FROM %s WHERE id = %s"   %(idAndType['typ'],idAndType['id'])
         cursor.execute(getpass)
         realPassword = cursor.fetchone()[0]
-        if (realPassword == password) : 
-            # authintication correct and making seasion
-            print("correct")
-            return render_template("loginForm.html",error=error)
+        print(realPassword , password)
+        if  realPassword != password : # somethin wrong here
+            # assign a name to session
+            session['username'] = request.form['id']
+            return redirect('/') # to home page (index)
         else : 
             error = "password incorrect"
             return render_template("loginForm.html",error=error)
-        
-    
-    
-    
-    
-    
